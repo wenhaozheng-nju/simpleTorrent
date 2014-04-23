f4eaa6b50a4fb8c57b4c0d6a95a1cef16570840b
YuhangZhou <zhouyuhangnju@gmail.com>
2014-04-23 02:12:14 -0700

    222222222222222222

--- src/listen_peers.c
+++ src/listen_peers.c
@@ -1,6 +1,17 @@
 #include "util.h"
 #include "btdata.h"
 
-void *listen_peers(){
-
+void *listen_peers(void *p){
+    int listenfd = make_listen_port(g_peerport);
+    while(1){
+        struct sockaddr_in cliaddr;
+        int clilen = sizeof(cliaddr);
+        int sockfd = accept(listenfd, (struct sockaddr*)&cliaddr, &clilen);
+        pthread_t thread;
+        int rc = pthread_create(&thread, NULL, recv_from_peer, (void *)sockfd);
+        if(rc){
+            printf("Error, return code from pthread_create() is %d\n", rc);
+            exit(-1);
+        }
+    }
 }
